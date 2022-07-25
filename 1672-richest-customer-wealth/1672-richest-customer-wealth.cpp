015fbdff043674452d5f398f2b32ef63fee28e33
haramchae <93884960+haramchae@users.noreply.github.com>
2022-07-25 13:08:06 -0700

    Time: 7 ms (66.21%), Space: 7.9 MB (49.90%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int wealth = -1, temp = 0;
-        for(int i = 0; i<accounts.size(); i++){
-            for(int j = 0; j<accounts[i].size(); j++){
-                temp = temp + accounts[i][j];
-            }
-            if(temp>wealth){
-                wealth = temp;
-            }
-            temp = 0;
+        for(auto &customer : accounts){
+            temp = accumulate(customer.begin(), customer.end(), 0);
+            wealth = max(wealth, temp);
         }
         return wealth;
         
