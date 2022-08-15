5d1e1a37bd0b1c3e02d2c12a95fae6445039372a
haramchae <93884960+haramchae@users.noreply.github.com>
2022-08-15 15:56:02 -0700

    Time: 27 ms (83.91%), Space: 19.4 MB (13.34%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()==1){
-            return;
-        }
-        int count = 0;
+        int current = 0; 
         for(int i = 0; i<nums.size(); i++){
-            if(nums[i]==0){
-                count++;
+            if(nums[i]!=0){
+                nums[current++]=nums[i];
             }
         }
-        nums.erase(remove(nums.begin(), nums.end(), 0), nums.end());
-        for(int i = 0; i<count; i++){
-            nums.push_back(0);
+        for(int i = current; i<nums.size(); i++){
+            nums[i]=0;
         }
         
     }
