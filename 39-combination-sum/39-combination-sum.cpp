ef40ce7d005d21425c125a380a1236c19faccfbb
haramchae <93884960+haramchae@users.noreply.github.com>
2022-08-22 16:35:28 -0700

    Time: 221 ms (13.95%), Space: 66.6 MB (15.89%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -8,17 +8,10 @@ public:
         return ans;
     }
     void helper(int target, int depth, int total, vector<int> candidates, vector<vector<int>>& ans, vector<int> temp){
-        if(depth > candidates.size()){
+        if(total > target){
             return;
-        }  
-        if(total == target){
-            sort(temp.begin(), temp.end());
-            auto check = find(ans.begin(), ans.end(), temp);
-            if(check == ans.end()){
-               ans.push_back(temp); 
-            }
-            return;
-        }else if(total > target){
+        }else if(total == target){
+            ans.push_back(temp);
             return;
         }
         
@@ -28,14 +21,6 @@ public:
             helper(target, i, total, candidates, ans, temp);
             temp.pop_back();
             total -= candidates[i];
-            if(i + 1 < candidates.size()){
-                temp.push_back(candidates[i+1]);
-                total += candidates[i+1];
-                helper(target, i+1, total, candidates, ans, temp);
-                temp.pop_back();
-                total -= candidates[i+1];
-            }
-            
         }
         
     }
