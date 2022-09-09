b83780bd7e6116a4b331848be09051e2ee110e7c
haramchae <93884960+haramchae@users.noreply.github.com>
2022-09-08 19:29:09 -0700

    Time: 15 ms (47.14%), Space: 8.9 MB (28.76%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        unordered_map<int, int> mp;
+        int ans = 0;
+        for(auto i: nums){
+            if(mp.find(i-diff) != mp.end() && mp.find(i-2*diff) != mp.end()) 
+                ans = ans + mp[i-diff]* mp[i-2*diff];
+            mp[i]++;
+        }
+        return ans;
+    }
+};
