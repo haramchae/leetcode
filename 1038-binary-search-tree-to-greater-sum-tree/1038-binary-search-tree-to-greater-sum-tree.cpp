e8b79e57bac4564f103e742cbd2a5e716786b85d
haramchae <93884960+haramchae@users.noreply.github.com>
2022-09-23 11:34:15 -0700

    Time: 0 ms (100.00%), Space: 8.2 MB (55.94%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,34 +11,20 @@
  */
 class Solution {
 public:
-    vector<int> v;
-    int index = 0;
+    int sum = 0;
     TreeNode* bstToGst(TreeNode* root) {
-        inorderHelper(root);
-        sumHelper(v);
         helper(root);
         return root;
     }
     
-    void inorderHelper(TreeNode* root){
-        if(!root) return;
-        inorderHelper(root->left);
-        v.push_back(root->val);
-        inorderHelper(root->right);
-    }
-    
-    void sumHelper(vector<int>& v){
-        for(int i = v.size()-2; i>=0; i--){
-            v[i] += v[i+1];
-        }
-    }
-    
     void helper(TreeNode* root){
         if(!root) return;
-        helper(root->left);
-        root->val = v[index++];
         helper(root->right);
+        root->val += sum;
+        sum = root->val;
+        helper(root->left);
     }
     
+   
     
 };
