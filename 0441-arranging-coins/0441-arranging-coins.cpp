dd64a6c0761a168e6c106528ea321948e84e3da0
haramchae <93884960+haramchae@users.noreply.github.com>
2022-10-30 13:38:08 -0700

    Time: 18 ms (20.29%), Space: 5.9 MB (25.51%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int row = 1;
+        while(n>0){
+            row++;
+            n -= row;
+        }
+        return row - 1;
+    }
+};
