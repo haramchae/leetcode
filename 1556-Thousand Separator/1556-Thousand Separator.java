d0f68d16ef2ac9d778f3a68e5e902146d5275e00
Haram Chae <cogkfka5@gmail.com>
2022-11-15 19:54:44 -0700

    Runtime 1 ms (84.89%) Memory 41.5 MB (41.31%)

--- 1556-Thousand Separator/1556-Thousand Separator.java
+++ 1556-Thousand Separator/1556-Thousand Separator.java
@@ -3,6 +3,7 @@ class Solution {
         String str = Integer.toString(n);
         int size = str.length();
         if(size<4) return str;
+        
         for(int i = size - 3; i > 0; i-=3){
             str = new StringBuffer(str).insert(i, ".").toString();
         }
