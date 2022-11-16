f5d965804e55842fa59b9b47b410cedc17772a7b
Haram Chae <cogkfka5@gmail.com>
2022-11-15 19:53:52 -0700

    Create 1556-Thousand Separator.java

---
+++ 1556-Thousand Separator/1556-Thousand Separator.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String thousandSeparator(int n) {
+        String str = Integer.toString(n);
+        int size = str.length();
+        if(size<4) return str;
+        for(int i = size - 3; i > 0; i-=3){
+            str = new StringBuffer(str).insert(i, ".").toString();
+        }
+        return str;
+    }
+}
