7eeb36df43f767ef90a0d50cb96a61f716423fd5
Haram Chae <cogkfka5@gmail.com>
2022-12-05 18:07:07 -0700

    Runtime 0 ms 100% Memory 40.7 MB 62.20%

---
+++ 1523-Count-Odd-Numbers-in-an-Interval-Range/1523-Count-Odd-Numbers-in-an-Interval-Range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int range = high-low+1;
+        if(range%2==0) return range/2;
+        if(low%2==0){
+            return range/2;
+        }
+        return range/2+1;
+    }
+}
