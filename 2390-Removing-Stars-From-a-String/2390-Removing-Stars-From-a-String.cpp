7474a9413fd952e44193799aa3065026909a12da
Haram Chae <cogkfka5@gmail.com>
2022-12-15 19:00:03 -0700

    Runtime 90 ms 90.15% Memory 24.1 MB 90.65%

--- 2390-Removing-Stars-From-a-String/2390-Removing-Stars-From-a-String.cpp
+++ 2390-Removing-Stars-From-a-String/2390-Removing-Stars-From-a-String.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     string removeStars(string s) {
-        string ans = "";
-        int check = 0;
-        stack<char> st;
-        for(auto it: s) st.push(it);
-        while(!st.empty()){
-            char temp = st.top();
-            if(temp == '*'){
-                check++;
-                st.pop();
-                continue;
-            }else{
-                if(check == 0){
-                    ans += temp;
-                    st.pop();
-                }else{
-                    st.pop();
-                    check--;
-                }
+        int j = 0; 
+        for(int i = 0; i<s.size(); ++i){
+            if(s[i]=='*') j--;
+            else{
+                s[j]=s[i];
+                j++;
             }
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return s.substr(0, j);
     }
 };
