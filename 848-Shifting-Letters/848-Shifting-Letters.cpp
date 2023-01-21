822709ec9b29457b81193c8c6862ac6e659b99b0
Haram Chae <cogkfka5@gmail.com>
2023-01-21 12:40:38 -0700

    Runtime 127 ms 96.5% Memory 67.9 MB 64.3%

--- 848-Shifting-Letters/848-Shifting-Letters.cpp
+++ 848-Shifting-Letters/848-Shifting-Letters.cpp
@@ -3,7 +3,7 @@ public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int sum = 0;
         for(int i = s.size()-1; i>=0; --i){
-            sum += shifts[i];
+            sum += shifts[i] % 26;
             s[i] = (s[i]-'a'+sum) % 26 + 'a';
         }
         return s;
