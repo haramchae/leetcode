76ee80609bb2538b050fb0a9249695251f6e63f4
Haram Chae <cogkfka5@gmail.com>
2023-01-28 14:18:13 -0700

    Runtime 99 ms 26.46% Memory 8.6 MB 33.27%

---
+++ 1155-Number-of-Dice-Rolls-With-Target-Sum/1155-Number-of-Dice-Rolls-With-Target-Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        int mod = 1000000007;
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=target; j++){
+                for(int u = 1; u<=k; u++){
+                    if(u<=j){
+                        dp[i][j] = ((dp[i][j]%mod) + (dp[i-1][j-u]%mod))%mod; 
+                    }
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
