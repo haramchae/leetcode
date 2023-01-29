d8b9e445ecda10ebaf8892cade2b0d310f2c384d
Haram Chae <cogkfka5@gmail.com>
2023-01-29 01:13:03 -0700

    Runtime 5 ms 63.24% Memory 41.8 MB 74.22%

--- 1005-Maximize-Sum-Of-Array-After-K-Negations/1005-Maximize-Sum-Of-Array-After-K-Negations.java
+++ 1005-Maximize-Sum-Of-Array-After-K-Negations/1005-Maximize-Sum-Of-Array-After-K-Negations.java
@@ -2,9 +2,18 @@ class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int sum = 0;
-        for(int it: nums) pq.add(it);
-        while(k-->0) pq.add(-pq.poll());
-        while(pq.size()>0) sum+=pq.poll();
+        for(int it: nums)
+        {
+            pq.add(it);
+        }
+        while(k-->0)
+        {
+            pq.add(-pq.poll());
+        }
+        while(pq.size()>0)
+        {
+            sum+=pq.poll();
+        }
         return sum;
     }
 }
