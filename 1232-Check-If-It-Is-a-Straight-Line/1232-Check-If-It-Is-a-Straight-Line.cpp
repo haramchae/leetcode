6ec00eabc32c77f4295f57fccec2e7c519659dca
Haram Chae <cogkfka5@gmail.com>
2023-02-06 14:37:15 -0700

    Runtime 15 ms 34.10% Memory 10.1 MB 80.3%

---
+++ 1232-Check-If-It-Is-a-Straight-Line/1232-Check-If-It-Is-a-Straight-Line.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int preX = coordinates[1][0]-coordinates[0][0];
+        int preY = coordinates[1][1]-coordinates[0][1];
+        for(int i = 2; i<coordinates.size(); ++i){
+            if((coordinates[i][0]-coordinates[0][0]) * preY !=  (coordinates[i][1]-coordinates[0][1]) * preX)
+                return false;
+        }
+        return true;
+    }
+};
