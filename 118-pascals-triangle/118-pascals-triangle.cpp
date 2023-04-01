a4111dcea90e46c71e550cc6f38697a1db03ad05
Haram Chae <cogkfka5@gmail.com>
2023-04-01 19:13:16 -0400

    Update 118-pascals-triangle.cpp

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> v;
+        
         for(int i = 0; i<numRows; i++){
             v.push_back(vector<int>(i+1, 1));
             for(int j = 1; j<i; j++){
