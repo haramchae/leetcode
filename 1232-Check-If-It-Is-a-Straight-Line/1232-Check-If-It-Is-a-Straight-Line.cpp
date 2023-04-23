cee7213f69d86eee8ec80ef562e40166182de5d7
Haram Chae <cogkfka5@gmail.com>
2023-04-22 23:35:16 -0400

    Update 1232-Check-If-It-Is-a-Straight-Line.cpp

--- 1232-Check-If-It-Is-a-Straight-Line/1232-Check-If-It-Is-a-Straight-Line.cpp
+++ 1232-Check-If-It-Is-a-Straight-Line/1232-Check-If-It-Is-a-Straight-Line.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int preX = coordinates[1][0]-coordinates[0][0];
-        int preY = coordinates[1][1]-coordinates[0][1];
+        int preX = coordinates[1][0] - coordinates[0][0];
+        int preY = coordinates[1][1] - coordinates[0][1];
         for(int i = 2; i<coordinates.size(); ++i){
             if((coordinates[i][0]-coordinates[0][0]) * preY !=  (coordinates[i][1]-coordinates[0][1]) * preX)
                 return false;
