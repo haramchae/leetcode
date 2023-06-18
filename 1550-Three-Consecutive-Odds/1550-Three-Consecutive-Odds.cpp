b8477e87a179685baf232e2014056da8499956d3
Haram Chae <cogkfka5@gmail.com>
2023-06-18 11:02:58 -0400

    Runtime 4 ms 69.17% Memory 8.4 MB 5.98%

---
+++ 1550-Three-Consecutive-Odds/1550-Three-Consecutive-Odds.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+        for(int num: arr){
+            if(num%2==1) count++;
+            else count = 0;
+            if(count == 3) return true;
+        }
+        return false;
+    }
+};
