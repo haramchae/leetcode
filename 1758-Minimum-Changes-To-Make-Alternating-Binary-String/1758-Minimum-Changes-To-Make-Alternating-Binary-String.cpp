b2f166777b59ebd40bd1a60822c98f52e85fad5f
Haram Chae <cogkfka5@gmail.com>
2023-07-26 23:00:35 -0400

    Runtime 3 ms 85.86% Memory 6.8 MB 94.61%

---
+++ 1758-Minimum-Changes-To-Make-Alternating-Binary-String/1758-Minimum-Changes-To-Make-Alternating-Binary-String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n=s.size(), ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i] - '0' != i%2)
+            ans++;
+        }
+        return min(ans, n-ans);
+    }
+};
